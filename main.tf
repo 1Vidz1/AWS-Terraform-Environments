58e6d485eeadc7e77cdb583907ac858e6ce05ec3
1Vidz1 <99675669+1Vidz1@users.noreply.github.com>
2022-02-14 17:49:14 +0000

    Terraform test enviroment

---
+++ main.tf
@@ -0,0 +1,33 @@
+//It's good practice to name the name file, main.tf
+provider "aws" {
+    region = "eu-west-1"
+}
+
+resource "aws_s3_bucket" "my-test-bucket" {
+  bucket = "terraform-bucket-101010"
+}
+
+resource "aws_instance" "web-bastion" {
+  ami           = "ami-0bf84c42e04519c85"
+  instance_type = var.instance_type
+  subnet_id = aws_subnet.public-sub-1.id
+  vpc_security_group_ids = [aws_security_group.allow_http_ssh.id]
+  key_name = var.keypair
+  associate_public_ip_address = true
+
+  user_data = <<-EOF
+  #!/bin/bash -ex
+  yum update -y
+  yum install -y httpd.x86_64
+  systemctl start httpd.service
+  systemctl enable httpd.service
+  echo “IF YOU CAN READ THIS, YOUR USER DATA IS CONFIGURED CORRECTLY” > /var/www/html/index.html
+  EOF
+
+  tags = {
+      Name = "WebHost"
+      env = var.enviroment
+      owner = "avidigal"
+  }
+}
+
---
+++ network.tf
@@ -0,0 +1,49 @@
+//Enviroment VPC
+resource "aws_vpc" "env-vpc" {
+  cidr_block = var.vpc
+}
+//Subnets
+resource "aws_subnet" "public-sub-1" {
+  vpc_id     = aws_vpc.env-vpc.id
+  cidr_block = var.pub1
+  availability_zone = "eu-west-1a"
+
+  tags = {
+    Name = "Public Subnet 1"
+  }
+}
+resource "aws_route_table_association" "public_1_rt_a" {
+  subnet_id      = aws_subnet.public-sub-1.id 
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_subnet" "private-sub-1" {
+  vpc_id     = aws_vpc.env-vpc.id
+  cidr_block = var.priv1
+  availability_zone = "eu-west-1a"
+
+  tags = {
+    Name = "Privat Subnet 1"
+  }
+}
+//Gateways
+resource "aws_internet_gateway" "int_gw" {
+  vpc_id = aws_vpc.env-vpc.id
+
+  tags = {
+    Name = "Internet Gateway"
+  }
+}
+//Route Tables
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.env-vpc.id 
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.int_gw.id
+  }
+
+  tags = {
+    Name = "Public Route Table"
+  }
+}
